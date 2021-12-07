15d575203e86b2e4aeed8595fd48abefb76c253e
swamymasna <swamymasna55@gmail.com>
2021-12-07 10:12:18 +0530

    Setter Injection Application

---
+++ 01-Spring-SDI-App/src/main/java/com/codebyswamy/beans/Employee.java
@@ -0,0 +1,43 @@
+package com.codebyswamy.beans;
+
+public class Employee {
+
+	private Integer empId;
+	private String empName;
+	private Double empSal;
+	
+	public Employee() {
+		super();
+	}
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public Double getEmpSal() {
+		return empSal;
+	}
+
+	public void setEmpSal(Double empSal) {
+		this.empSal = empSal;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + "]";
+	}
+	
+	
+}
