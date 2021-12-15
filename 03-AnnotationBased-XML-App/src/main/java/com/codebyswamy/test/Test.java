ea273252246d01609d9f7b37396e196b716a830c
swamymasna <swamymasna55@gmail.com>
2021-12-15 07:23:11 +0530

    Annotation Config-XML-Based-App

---
+++ 03-AnnotationBased-XML-App/src/main/java/com/codebyswamy/test/Test.java
@@ -0,0 +1,20 @@
+package com.codebyswamy.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.codebyswamy.beans.Company;
+
+public class Test {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Company company = context.getBean(Company.class);
+		company.setCmpId(5455);
+		company.setCmpName("SUZUKI");
+		company.setCmpProf(5600.00);
+		System.out.println(company);
+	}
+}
+
