03137a56a14fcbb9a0d324affd07d2d10d4a1907
swamymasna <swamymasna55@gmail.com>
2022-01-18 09:09:00 +0530

    P & C- Namespace Application

---
+++ 13_P_and_C_Namespace_App/src/main/java/com/codebyswamy/test/Test.java
@@ -0,0 +1,17 @@
+package com.codebyswamy.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.codebyswamy.beans.Employee;
+
+public class Test {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Employee employee = context.getBean(Employee.class);
+		System.out.println(employee);
+	}
+}
+
