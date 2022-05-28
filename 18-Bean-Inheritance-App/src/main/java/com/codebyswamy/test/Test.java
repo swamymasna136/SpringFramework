0d3d06189adbad2949695f991404fa78ffce213f
swamymasna <swamymasna55@gmail.com>
2022-05-28 07:03:50 +0530

    Bean Inheritance

--- 18-Bean-Inheritance-App/src/main/java/com/codebyswamy/test/Test.java
+++ 18-Bean-Inheritance-App/src/main/java/com/codebyswamy/test/Test.java
@@ -3,15 +3,15 @@ package com.codebyswamy.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.codebyswamy.beans.Employee;
+import com.codebyswamy.beans.Car;
 
 public class Test {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		Employee employee = context.getBean(Employee.class);
-		System.out.println(employee);
+		Car car = context.getBean("ertiga2" , Car.class);
+		System.out.println(car);
 	}
 }
 
