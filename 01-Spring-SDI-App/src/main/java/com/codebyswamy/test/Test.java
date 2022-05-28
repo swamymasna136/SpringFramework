369b913c87bb389ce3e8db819386755962e69df2
swamymasna <swamymasna55@gmail.com>
2022-05-28 07:23:05 +0530

    Setter Injection Application

--- 01-Spring-SDI-App/src/main/java/com/codebyswamy/test/Test.java
+++ 01-Spring-SDI-App/src/main/java/com/codebyswamy/test/Test.java
@@ -7,7 +7,7 @@ import com.codebyswamy.beans.Employee;
 
 public class Test {
 
-	@SuppressWarnings("resource")
+	
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Employee employee = context.getBean(Employee.class);
