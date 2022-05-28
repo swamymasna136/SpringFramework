e9c6f004aa2bba65c2932f33cdc67bc47c231f1f
swamymasna <swamymasna55@gmail.com>
2022-05-28 07:37:08 +0530

    Annotation Config-XML-Based-App

--- 03-AnnotationBased-XML-App/src/main/java/com/codebyswamy/test/Test.java
+++ 03-AnnotationBased-XML-App/src/main/java/com/codebyswamy/test/Test.java
@@ -11,7 +11,7 @@ public class Test {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Company company = context.getBean(Company.class);
-		company.setCmpId(5455);
+		company.setCmpId(5465);
 		company.setCmpName("SUZUKI");
 		company.setCmpProf(5500.00);
 		System.out.println(company);
