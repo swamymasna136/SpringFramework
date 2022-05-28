c8cf03c48589d14f4a6677db49eca20efd0f0653
Swamy Masna <93122553+swamymasna136@users.noreply.github.com>
2022-05-28 08:20:59 +0530

    Update Test.java

--- 17-ReadingData-FromPropsFile/src/main/java/com/codebyswamy/test/Test.java
+++ 17-ReadingData-FromPropsFile/src/main/java/com/codebyswamy/test/Test.java
@@ -14,6 +14,7 @@ public class Test {
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		Product product = context.getBean(Product.class);
 		System.out.println(product);
+		System.out.println(product);
 	}
 }
 
